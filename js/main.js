e5ccc90ad5daeb623d467a30f761386d8df09bdc
Abbey <abigail.hartvigsen>
2019-09-04 17:26:01 -0600

    fixed reDisplay function

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function reDisplay () {
 let windowSizes = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`
 let offset = `Window offset is ${window.screenX} pixels from the left edge and ${window.screenY} pixels from the top of the display.`
 
-let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset}`
+document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset}`
 }
-reDisplay
+reDisplay();
 
