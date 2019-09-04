6fce936893c7ff4332601a91dfc98826234e19cc
Abbey <abigail.hartvigsen>
2019-09-03 22:14:49 -0600

    Document.queryselector h1&h2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,2 @@
-// function reDisplay() {
-//     var txt = 'Window size is '+window.innerWidth+" wide by "+ window.innerHeight+" tall.<br>";
-//     txt += 'Window offset is '+window.screenX+" from the left edge and "+ window.screenY+" from the top of the display.<br>";
-//     txt+="The page URL is "+window.location+"<br>";
-//     document.getElementById("myWindow").innerHTML = txt;
-
-//     var txt2 = 'Document title is '+document.title+"<br>";
-//         txt2 += 'This document was last updated on '+document.lastModified+"<br>";
-//         document.getElementById("myDocument").innerHTML = txt2;
-// } 
-// reDisplay();
-
 document.querySelector('header > h1').innerText = "Inspector Clouseau"
+document.querySelector('header > h2').innerText = "My name is Inspector Clouseau"
