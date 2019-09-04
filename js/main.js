fd5c976bf6071eca2f9bd6eeb2d4eb300f8f45a6
Abbey <abigail.hartvigsen>
2019-09-04 13:15:27 -0600

    reDisplay function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 document.querySelector('header > h1').innerText = "Inspector Clouseau"
 document.querySelector('header > h2').innerText = "My name is Inspector Clouseau"
 
+function reDisplay () {
 let windowSizes = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`
 let offset = `Window offset is ${window.screenX} pixels from the left edge and ${window.screenY} pixels from the top of the display.`
 
-let myWindow = document.querySelector('#myWindow').innerText = windowSizes + '\n' + offset
+let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset}`
+}
+reDisplay
+
