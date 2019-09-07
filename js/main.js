1af9bd5d3ddfa8a17b20dba4b2a6940b5f5ed749
Abbey <abigail.hartvigsen>
2019-09-06 23:04:41 -0600

    Document properties info and url and footer color

--- js/main.js
+++ js/main.js
@@ -4,8 +4,15 @@ document.querySelector('header > h2').innerText = "My name is Inspector Clouseau
 function reDisplay () {
 let windowSizes = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`
 let offset = `Window offset is ${window.screenX} pixels from the left edge and ${window.screenY} pixels from the top of the display.`
+let url = `The page URL is ${document.URL}`
 
-document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset}`
+document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset} \n ${url}`
 }
 reDisplay();
+
+let documentTitle = `Document title is ${document.title}`
+let documentUpdate = `This document was last updated ${document.lastModified}`
+
+document.querySelector('#myDocument').innerText = `${documentTitle} \n ${documentUpdate}`
+
 
